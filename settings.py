e41d281d0f8a9456c3edce9262ddaee2976b87c6
sairam101 <72657580+sairam101@users.noreply.github.com>
2021-09-24 09:55:42 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,3 @@
+TRACK_WORDS = ["#terroristattack","#terrorism","#terrorists","#Uri","#Pulwama","#MumbaiAttack","#2611Attack","#binladen","#AlQaeda","#Taliban","#alkayda","#ISIS","Terrorism_targeting_India","#assassination", "#bloodshed", "#crime", "#destruction", "#felony","#homicide", "#lynching","#manslaughter","#massacre", "#shooting","#slaying","#annihilation","#bombblast","#rdx","#ak47" ,"#gun","#pistool", "#Al-Qaeda", "#Lashkar-e-Omar","#Lashkar-e-Taiba", "#LeT", "#Jaish-e-Mohammed","#JeM", "#Sipah-e-Sahaba", "#Jaish-ul-Adl" ,"#AlBadrMujahideen","#Harkat-ul Mujahideen", "#ISIS-KP", "#Pakistani", "#DawoodIbrahim", "#MasoodAzhar"]
+TABLE_NAME = "TwitterDataset"
+TABLE_ATTRIBUTES = "id VARCHAR(255), created_at DATETIME, text VARCHAR(4000)"
